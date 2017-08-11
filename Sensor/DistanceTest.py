a9af24a14124e6130bd57d95029ebdde7f6131f9
Paidin <patrick.schange@posteo.ch>
2017-08-11 15:01:40 +0200

    Create DistanceTest.py
    Basierend auf Sensor/Distance.py, kann mit diesem Skript eine Messreihe über 30 Messungen
    angelegt werden, wobei die Messdaten des HC-SR04 Sensors in einer Datei gespeichert werden.
    So können sie später nach Excel kopiert und relativ einfach analysiert werden.

---
+++ Sensor/DistanceTest.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Basierend auf Sensor/Distance.py, kann mit diesem Skript eine Messreihe über 30 Messungen
+angelegt werden, wobei die Messdaten des HC-SR04 Sensors in einer Datei gespeichert werden.
+So können sie später besser analysiert werden.
+"""
+
+from Distance import *
+
+import time
+import statistics as stat
+
+# Initialisiere einige Variabeln
+tgtDistance = int(input("Soll-Abstand zum Hindernis [cm] >> "))
+height = float(input("Höhe des Sensors [cm] >>"))
+addition = str(input("Dateinamenszusatz >> "))
+Log = open("Messung-" + str(tgtDistance) + "-" + addition, "wt")
+print("Sensorhoehe: " + str(height) + " cm", file=Log)
+print("Soll-Abstand: " + str(tgtDistance) + " cm", file=Log)
+print("Messdaten [cm]: ", file=Log)
+
+# Initialisiere Pins und führe eine erste Messung durch
+# erste Messung erhält nie eine brauchbare Antwort des Sensors
+initPins()
+getDistance()
+
+# Führe die Messreihe aus
+data = []
+for i in range(0, 30):
+    distance = getDistance()
+    print("%.4f" % distance, file=Log)
+    data.append(distance)
+    time.sleep(1)
+    
+# Beende die Messungen
+clearPins()
+Log.close()
+
+# Gebe eine kurze Zusammenfassung der Messreihe auf dem Bildschirm aus
+median = stat.median(data)
+offset = median - tgtDistance
+print("\n===========================================")
+print("Zusammenfassung der Messdaten\n")
+print("Sensorhöhe [cm]:", height)
+print("Soll-Abstand [cm]:", tgtDistance)
+print("Median der Messreihe [cm]:", median)
+print("Abweichung vom Soll [cm]:", offset)
+print("Abweichung vom Soll  [%]:", offset/tgtDistance*100)
+print("===========================================")
+    
