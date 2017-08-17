c7edf3ad8c8e6ea46386dedab1048187adddbddc
Paidin <patrick.schange@posteo.ch>
2017-08-17 19:07:19 +0200

    Update 298einmotor.py
    Entfernung von fehlerhaften Einschüben

--- 298einmotor.py
+++ 298einmotor.py
@@ -3,16 +3,15 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 def all_off(): 
-GPIO.output(EnableA, False) 
-GPIO.output(Input1,False) 
-GPIO.output(Input2,False) 
+	GPIO.output(EnableA, False) 
+	GPIO.output(Input1,False) 
+	GPIO.output(Input2,False) 
 
 
 #Namen könnten bei unserem Motortreiber anders sein
 EnableA = 13 
 Input1 = 3 
 Input2 = 5 
-
 
 GPIO.setup(EnableA,GPIO.OUT) 
 GPIO.setup(Input1,GPIO.OUT) 
@@ -21,12 +20,10 @@ GPIO.setup(Input2,GPIO.OUT)
 all_off()
 
 try:
-	while True :
-	
-	richtung =
-		input(" Geben sie Drehrichtung des Motors an: 'V' oder 'Z': ")
+	while True :	
+		richtung = input(" Geben sie Drehrichtung des Motors an: 'V' oder 'Z': ")
 		
-	if richtung == "V" :
+		if richtung == "V" :
 			GPIO.output(EnableA, True)
 			GPIO.output(Input1, True) 
 			GPIO.output(Input2, False)
@@ -34,7 +31,7 @@ try:
 			GPIO.output(EnableA, False) 
 			GPIO.output(Input1, False)
 			
-	if richtung =="Z"  :
+		if richtung =="Z"  :
 			GPIO.output(EnableA, True)
 			GPIO.output(Input1, False)
 			GPIO.output(Input2, True)
@@ -43,5 +40,5 @@ try:
 			GPIO.output(Input2, False)
 		
 		
-		except KeyboardInterrupt : 
-		all_off()
+except KeyboardInterrupt : 
+	all_off()
