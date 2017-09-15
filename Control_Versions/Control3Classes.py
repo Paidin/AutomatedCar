7cf909922035f52de22951a3edf7495d8c1eecad
Paidin <patrick.schange@posteo.ch>
2017-09-15 18:24:34 +0200

    Create Control3Classes.py
    Genau wie Control2.py, aber die Funktionen für die Streuerung des Sensors und des Motors sind in einer Klasse verpackt

---
+++ Control_Versions/Control3Classes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Skript zur Anpassung der Geschwindigkeit des Motors in Abhängigkeit
+    zur Entfernung des vorausfahrenden Fahrzeuges
+"""
+
+import time
+import Sensor
+import Motor
+
+
+# Ziel-Entfernung, die eingehalten werden soll
+tgtDistance = 30 
+
+# Aktuelle Geschwindigkeit, ausserhalb der Klasse gespeichert
+speed = 0
+
+# Initialisiere Ultraschallsensor
+sensor = Sensor.Sensor()
+sensor.getDistance()
+
+# Initialisiere Motor mit maximalem Tastgrad von 30%
+motor = Motor.Motor(30)
+motor.changeSpeed(int(0))
+
+
+try:
+    while True:
+        # Ermittle Entfernung zum Hindernis
+        dist = sensor.getDistance()
+        time.sleep(0.01)
+        
+        # Ignoriere ungültige Messungen
+        if dist is None:
+            continue
+        
+        # Änderung der Geschwindigkeit
+        if dist > tgtDistance:
+            motor.changeSpeed(int(5))
+        if dist < tgtDistance:
+            motor.changeSpeed(int(-5))
+        
+        pass
+    
+except:
+    print("Ok, something went wrong...")
+    print("or someone pressed ^C")
+    
+finally:
+    motor.stopMotor()
+        
+
+
+
