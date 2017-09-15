6b77229ab85bb877533a3777188628ada71544c9
Paidin <patrick.schange@posteo.ch>
2017-09-15 17:43:39 +0200

    Update Control_V1.py

--- Control_Versions/Control_V1.py
+++ Control_Versions/Control_V1.py
@@ -1,9 +1,8 @@
 """
 	Skript um Entfernungsmessungen und Motorgeschwindigkeit zu kombinieren
+	in Abhängigkeit der relativen Geschwindigkeit zum vorausfahrenen Fahrzeug
 """
 
-
-import RPi.GPIO as GPIO
 import time
 
 # Modul als Klasse schreiben ???
@@ -18,27 +17,12 @@ Distance.initPins()
 Distance.getDistance()
 
 Motor.maxVelocity = 30
-Motor.changeVelocity(float(0))
+Motor.changeVelocity(int(0))
 input("Press Enter to continue >> ")
 
-"""
-# alter Code, ohne Verwendung unserer Funktionen in Distance.py und MotorFunctions.py
-try:
-	while checker == true
-		distance1 = getDistance()
-		time.sleep (0.1)
-		distance2 = getDistance()
-		dutyCycle = ((distance1 - distance2) / 0.1)*x
-		if dutyCycle > 30
-			dutyCycle = 30
-		if dutyCycle = 0
-			checker = false
-		pwm.ChangeDutyCycle(float(dutyCycle))
-"""
-
-# neuer Code
 try:
 	while True:
+		# Entfernungsmessungen
 		dist1 = Distance.getDistance()
 		t1 = time.time()
 		time.sleep(0.01)
@@ -47,15 +31,15 @@ try:
 		
 		if dist1 == None or dist2 == None:
 			continue
-		
-		# Mathematik dahinter überprüfen bitte :)
+		# Problem:
+		# bei kleinen Messungenauigkeiten wird durch das extrem kleine delta t
+		# eine grosse Geschwindigkeit errechnet, auch wenn Vrelativ = 0 sein sollte
 		speed = ((dist1 - dist2) / (t1 - t2))*x
-		Motor.changeVelocity(float(speed))
-		
+		Motor.changeVelocity(int(speed))		
 			
 except:
 	print("Ok, something went wrong...")
 
 finally:
-	Motor.changeVelocity(float(0))
+	Motor.changeVelocity(int(0))
 	Motor.stopMotor()
